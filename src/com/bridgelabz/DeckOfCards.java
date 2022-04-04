794f5043a8ca059872fad2163afa4f86ba623421
Amol Ghotale <ghotaleamol@gmail.com>
2022-04-04 21:26:51 +0530

    ADDED: Sequence of play

--- src/com/bridgelabz/DeckOfCards.java
+++ src/com/bridgelabz/DeckOfCards.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DeckOfCards {
+    //  static Scanner scanner = new Scanner(System.in);
+    // static int players = scanner.nextInt();
+
     ArrayList<String> deck = new ArrayList<>();
     public static final int TOTAL_CARDS = 52;
+
 
     public void deckOfCards() {
         System.out.println("Welcome to deck of cards game.");
@@ -20,26 +24,35 @@ public class DeckOfCards {
         }
         System.out.println("Total cards inside deck are: " + TOTAL_CARDS);
         System.out.println("Cards inside the deck....");
+
         for (String element : deck) {
             System.out.println(element);
         }
     }
 
-    public void noOfPlayers() {
-        Scanner scanner = new Scanner(System.in);
+    public int noOfPlayers() {
+        Scanner scan = new Scanner(System.in);
         System.out.println("\nEnter the no. of players");
-        int players = scanner.nextInt();
-
+        int players = scan.nextInt();
         if (players > 2 && players <= 4) {
             System.out.println("The game starts");
         } else {
             System.out.println("Required players to play the game are more than 2 and Max 4");
+        }
+        return players;
+    }
+
+    public void playersSequence(int players) {
+        System.out.println("Distribution of cards will be as follows: ");
+        for (int i = 1; i <= players; i++) {
+            System.out.println("Player " + i + " getting cards------>");
         }
     }
 
     public static void main(String[] args) {
         DeckOfCards cardsdeck = new DeckOfCards();
         cardsdeck.deckOfCards();
-        cardsdeck.noOfPlayers();
+        int members = cardsdeck.noOfPlayers();
+        cardsdeck.playersSequence(members);
     }
 }
